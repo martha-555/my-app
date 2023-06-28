bb07f46fa369e17142a54da5d31faf691aeadd0e
martha-555 <martagolov4ak@gmail.com>
2023-06-28 16:50:26 +0300

    fixed onClick

--- src/pages/AddTrackToFavorite/AddTrackToFavorite.tsx
+++ src/pages/AddTrackToFavorite/AddTrackToFavorite.tsx
@@ -5,7 +5,6 @@ import useDeezerRequest from "../../feautures/api/hooks/deezer/useDeezerRequest"
 import { HttpMethod } from "../../feautures/api/types";
 import { TrackData } from "../../types/deezer";
 import useFetchUsersPlaylists from "../../feautures/api/hooks/deezer/useFetchUsersPlaylists";
-import useFetchFavoriteTracks from "../../feautures/api/hooks/deezer/useFetchFavoriteTracks";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import classes from './styles.module.scss'
 import { parseDeezerTrack } from "../../utils/deezer";
@@ -21,12 +20,11 @@ type Props = {
   tracks: TrackData[];
   error: string;
 };
-//   `/playlist/11185422824/tracks&songs=${e.target.id}`,
+
 // /user/me/playlists&title=namr
 const AddTrackToFavorite = ({ tracks, error }: Props) => {
 const [lovedTracks, setLovedTracks] = useState<number>()
 const [onClick, setOnClick] = useState<boolean> (false)
-const [clickLike, setClickLike] = useState<boolean>(false)
 const [selectedTrack, setSelectedTrack] = useState(0)
 const [idLikedList, setidLikedList] = useState<number[]>([])
 const [idLiked,setIdLiked] = useState<number>(0)
@@ -39,15 +37,15 @@ const request = useDeezerRequest();
   const handleClick = (e:React.MouseEvent<HTMLButtonElement>  ) => {
     const target = e.target as HTMLButtonElement;
     setIdLiked(+target.id)
-
     const fetchRequest = async () => {
-      await request(`/playlist/${lovedTracks}/tracks&songs=${target.id}`, idLikedList.includes(+target.id)? HttpMethod.DELETE: HttpMethod.POST );
+      await request(`/playlist/${lovedTracks}/tracks&songs=${target.id}`, idLikedList.includes(+target.id) && idLikedList? HttpMethod.DELETE: HttpMethod.POST );
     };
     fetchRequest();
-    +target.id === +idLiked? setClickLike(!clickLike): setClickLike(true)
+   
+    
   };
 
-// useEffect(() => {setClickLike(true)},[idLiked])
+
 
 useEffect(() => {
  playlists.map((item)=> item.title == 'Loved Tracks'? setLovedTracks(item.id): null)
@@ -68,9 +66,9 @@ setOnClick(!onClick)
 
       setidLikedList(id)
     };
-
     fetchRequest();
-  }, [authKey, request,idLiked]);
+  }, [authKey, handleClick,idLiked]);
+
 
   return (
     <div>
@@ -78,7 +76,7 @@ setOnClick(!onClick)
         tracks.map((item) => (
           <Track track={item} key={item.id}> 
 
-            <button id={item.id.toString()} onClick={handleClick} className={(+idLiked === +item.id && clickLike) || idLikedList.includes(+item.id)  ?classes.isLiked:''}>
+            <button id={item.id.toString()} onClick={handleClick} className={ idLikedList.includes(+item.id)?classes.isLiked:''}>
           	&#10084;  
             </button>
           <button  id={item.id.toString()} onClick={selectPlaylist} >Додати в плейлист</button>
