c8d858d33096f65d2426482a56ca69705826e4aa
martha-555 <martagolov4ak@gmail.com>
2023-08-11 14:11:05 +0300

    1108

--- src/pages/Playlists/CreatePlaylists.tsx
+++ src/pages/Playlists/CreatePlaylists.tsx
@@ -9,7 +9,12 @@ import useFetchTrackList from "../../feautures/api/hooks/deezer/useFetchTrackLis
 import Tracklist from "../../components/Tracklist/Tracklist";
 import { useSearchParams } from "react-router-dom";
 import DeletePlaylist from "./DeletePlaylist";
+import SearchTracks from "../Search/SearchTracks";
 
+type Props = {
+    trackList: TrackData[];
+    empty:string
+}
 
 const CreatePlaylists = () => {
     const deezerRequest = useDeezerRequest()
@@ -18,7 +23,9 @@ const CreatePlaylists = () => {
     const [idNewPlaylist, setIdNewPlaylist] = useState<number>(0)
     const [allPlaylists, setAllPlaylists] = useState<Playlist[]>([])
     const [searchParams, setSearchParams] = useSearchParams({});
+    // const [trackList, settracklist] = useState<TrackData[]>([])
     const trackList =  useFetchTrackList({path:`/playlist/${searchParams.get('playlist')}`});
+    const request = useDeezerRequest();
 
     const handleClick =() => {
         createPlaylist({name,setState:setIdNewPlaylist,request:deezerRequest}); 
@@ -33,29 +40,44 @@ if (target.className.includes('optionButton') === false) setSearchParams( {'play
 
 
 useEffect(() => {
-    if (idNewPlaylist) setSearchParams({'playlist': idNewPlaylist.toString()});
-},[idNewPlaylist])
+    // trackList?.length === 0 && searchParams.get("playlist")? setEmpty("Цей плейлист пустий") : setEmpty('');
+},[searchParams,request])
 
+if (idNewPlaylist) setSearchParams({'playlist': idNewPlaylist.toString()});
 useEffect(() => {
     fetchUsersPlaylists({request:deezerRequest,setState:setAllPlaylists});
    searchParams.get('playlist') && trackList?.length === 0? setEmpty('Цей плейлист пустий'): setEmpty('');
-},[trackList,idNewPlaylist,])
-    
+},[trackList,idNewPlaylist])
+
+
+// useEffect(() => {
+//       const requestFetch = async () => {
+//       const response = await request(
+//       (`/playlist/${searchParams.get('playlist')}`)
+//           );
+//           const list = await response.json();
+//       list?.tracks?.data?.length === 0 && searchParams.get("playlist")? setEmpty("Цей плейлист пустий") : setEmpty('');
+//     settracklist(list?.tracks?.data);
+//       };
+//       requestFetch()
+//   },[searchParams,request])
+
     return(
         <div className={classes.container}>
             <PageWrapper>
                 <div className={(trackList?.length > 0 && searchParams.get('playlist')) || empty ? classes.hide : ''}>
-<input value={name} type="text" onKeyUp={(e) =>{ if (e.key === "Enter") handleClick() }} onInput={(e) => {setName((e.target as HTMLInputElement).value)}} />
-<button onClick={handleClick}>create playlist</button>
-<div className={classes.playlistsContainer}>
+                    <div><input value={name} type="text" onKeyUp={(e) =>{ if (e.key === "Enter") handleClick() }} onInput={(e) => {setName((e.target as HTMLInputElement).value)}} />
+<button onClick={handleClick}>create playlist</button><div className={classes.playlistsContainer}>
     { allPlaylists?.map((item) =><div id={item.id.toString()} className={classes.playlists} key={item.id} onClick={clickedPlaylist}><div id={item.id.toString()}>{item.title}</div><DeletePlaylist playlistId={item.id} setState={setAllPlaylists}/> </div> )}
-    </div>
+    </div></div> :<div>
+ </div>   
+   
+
+
 </div>
- {trackList?
- <div>
-    <Tracklist tracks={trackList} error=""/>
- </div>  : null }
- {empty? <div>{empty} </div>: null }
+
+<Tracklist tracks={trackList} error={empty}/>
+ {/* {empty? <div>{empty} </div>: null } */}
             </PageWrapper>
         </div>
     )
