ada21d9d3263719bd176e04324e54e99b47e9c50
martha-555 <martagolov4ak@gmail.com>
2023-12-04 11:32:30 +0200

    0412

--- src/layout/PageWrapper/PageWrapper.tsx
+++ src/layout/PageWrapper/PageWrapper.tsx
@@ -5,19 +5,23 @@ import Menu from "../Menu";
 import classes from "./styles.module.scss";
 import Player from "../../components/Player/Player";
 import SearchTracks from "../../pages/Search/SearchTracks";
+import { useNavigate } from "react-router";
 
 type Props = {
   children: ReactNode;
 };
 
 const PageWrapper = ({ children }: Props) => {
+  const navigate = useNavigate()
   return (
     <div className={classes.container}>
       <Menu />
       <div className={classes.rightSide}>
         <div className={classes.content}>
-          <input></input>
-          {children}{" "}
+          {/* <input onInput={()=> navigate('/search')}></input>
+          <button onClick={()=> navigate('/search')}>Ok</button> */}
+          <SearchTracks></SearchTracks>
+          {children}
         </div>
         <Player />
       </div>
