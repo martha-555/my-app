0868b7c080f6cba1f238a7432b6ca802ec615398
martha-555 <112080638+martha-555@users.noreply.github.com>
2025-01-13 20:52:22 +0200

    push

--- src/utils/splicedTracks.ts
+++
@@ -1,9 +0,0 @@
-// import { TrackData } from "../types/deezer";
-
-// export const getSplicedTracks = (response:TrackData[]):TrackData[][] => {
-//     const spliceResponse = []
-//     while (response.length) {
-//       spliceResponse.push(response.splice(0,24));
-// }
-// return spliceResponse;
-// }
