67bf55ded9d465af7bb62ddc1fe659b2f665639c
martha-555 <112080638+martha-555@users.noreply.github.com>
2025-01-17 00:34:26 +0200

    changed backend_url

--- src/feautures/api/hooks/useBackendRequest.ts
+++ src/feautures/api/hooks/useBackendRequest.ts
@@ -1,10 +1,11 @@
+/** @format */
 
 "use client";
 import { useCallback, useState } from "react";
 import { BackendRequestBody } from "../types";
 import { useErrorBoundary } from "react-error-boundary";
 
-const BACKEND_URL = "http://localhost:3001";
+const BACKEND_URL = "https://music-backend-roan.vercel.app";
 
 export type BackendResponseParser<Data> = (response: Response) => Promise<Data>;
 export type BackendRequestState<Data> =
@@ -26,45 +27,41 @@ const useBackendRequest = <Data>(): UseBackendRequestReturn<Data> => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<boolean>(false);
 
-//catch(e) { setIsLoading(true)
-// return Promise.resolve(null)
-// }
+  //catch(e) { setIsLoading(true)
+  // return Promise.resolve(null)
+  // }
 
-  const makeRequest: RequestMaker<Data>= useCallback(
-    async (body: BackendRequestBody, parser: BackendResponseParser<Data | null>) => {
+  const makeRequest: RequestMaker<Data> = useCallback(
+    async (
+      body: BackendRequestBody,
+      parser: BackendResponseParser<Data | null>
+    ) => {
       setData(null);
       setIsLoading(true);
-  
-  const data = 
-    await fetch(BACKEND_URL, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    const parsedData = await parser(data);
 
-    setData(parsedData);
-    setIsLoading(false);
+      const data = await fetch(BACKEND_URL, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    return parsedData;
+      const parsedData = await parser(data);
 
-      
+      setData(parsedData);
+      setIsLoading(false);
+
+      return parsedData;
     },
     [setIsLoading]
   );
 
-  if (isLoading ) {
+  if (isLoading) {
     return [makeRequest, { isLoading: true, data: null }];
   }
 
   return [makeRequest, { isLoading: false, data: data as Data }];
-
-
-
-
 };
 
 export default useBackendRequest;
